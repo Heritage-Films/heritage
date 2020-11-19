30b7f0ac3f318283696df5e1fbb908223e8186b6
Jonathan Redford <jonathanredford@Jonathans-iMac.local>
2020-11-19 16:07:31 +1000

    master update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
-    // library: "HeritageSegment",
+    library: "HeritageAnalytics",
     libraryTarget: "umd",
   },
   module: {
