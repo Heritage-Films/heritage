2afbfb3da6c2a55a314d304176d670bcaffad783
Jonathan Redford <jonathanredford@Jonathans-iMac.local>
2020-11-24 15:11:10 +1000

    master update

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,21 @@
 const path = require("path")
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.js"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-        library: "HeritageAnalytics",
-        libraryTarget: "umd",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                        plugins: [
-                            ["@babel/plugin-transform-async-to-generator", {}],
-                            ["@babel/plugin-transform-arrow-functions", {}],
-                            ["@babel/plugin-transform-modules-commonjs", {}],
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-    mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    library: "HeritageAnalytics",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  mode: "production",
 }
