b568a74f8eff30ce49fa2b8977149bfa0c259e88
Jonathan Redford <jonathanredford@Jonathans-iMac.local>
2020-12-15 12:25:44 +1000

    master update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import initMiddleware from './initMiddleware'
 import identifyUser from './identifyUser'
-import helpscout from './helpscout'
 import init from './init'
 
 export {
