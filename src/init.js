c0406c37fc749b7450b3f4c1e1c35648bb51df73
Jonathan Redford <jonathanredford@Jonathans-iMac.local>
2020-12-15 12:18:40 +1000

    master update

--- src/init.js
+++ src/init.js
@@ -6,6 +6,16 @@ const libs = {
     helpscout
 }
 
-export default function(lib) {
-    libs[lib].call()
+export default function(x) {
+    try {
+        if(Array.isArray(x)) {
+            for(var i =0; i < x.length; i++) {
+                libs[x[i]].call()
+            }
+        } else {
+            libs[x].call()
+        }
+    } catch(ex) {
+        console.log(ex)
+    }
 }
