d66dea08f6500423c584d9fb875d54750cbea444
Jonathan Redford <jonathanredford@Jonathans-iMac.local>
2020-12-18 07:39:47 +1000

    master update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import init from './libs/init'
 
+function someFunction() {
+    alert('This is a very basic function that triggers an alert.')
+}
 export {
-    init
+    someFunction
 }
